25b3c8ff42b77e073817d487027e9ad22b308ac3
[MEADOW_DEV} <bendixon50@gmail.com>
2016-10-13 09:17:36 +0100

    Added position to config file

--- app/config.js
+++ app/config.js
@@ -3,6 +3,8 @@ const config = require('electron-config');
 module.exports = new config({
     defaults: {
         lastWindowState: {
+            x: 0,
+            y: 0,
             width: 1280,
             height: 720
         }
